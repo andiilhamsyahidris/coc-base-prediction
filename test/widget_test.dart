31952e0ab4524987f7c4b38fa088a7591a0f433c
iyan <andi.ilhamsyah@digi-id.id>
2024-05-27 11:58:21 +0700

    init architecture mvvm

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:coc_base_prediction/main.dart';
+import 'package:coc_base_prediction/app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
